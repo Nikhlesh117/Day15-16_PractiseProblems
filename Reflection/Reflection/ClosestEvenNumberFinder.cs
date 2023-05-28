cd84fc531c6f4460afe7b48a833d6d977a74f504
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-28 12:11:13 +0530

    Added: Find The Number

---
+++ Reflection/Reflection/ClosestEvenNumberFinder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class ClosestEvenNumberFinder
+    {
+        public int FindClosestEvenNumber(int N)
+        {
+            string strNum = Math.Abs(N).ToString();
+            int closestNum = N;
+            int minDiff = int.MaxValue;
+
+            foreach (char digit in strNum)
+            {
+                int num = int.Parse(digit.ToString());
+
+                if (num % 2 != 0)
+                {
+                    num++; // Make the digit even
+                }
+
+                int newNum = int.Parse(strNum.Replace(digit, num.ToString()[0]));
+
+                int diff = Math.Abs(newNum - Math.Abs(N));
+
+                if (diff < minDiff || (diff == minDiff && newNum < closestNum))
+                {
+                    minDiff = diff;
+                    closestNum = newNum;
+                }
+            }
+
+            if (N < 0)
+            {
+                closestNum *= -1; // Maintain the negative sign
+            }
+
+            return closestNum;
+        }
+    }
+}
---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int N = 17;
+            ClosestEvenNumberFinder finder = new ClosestEvenNumberFinder();
+            int closestNum = finder.FindClosestEvenNumber(N);
+            Console.WriteLine($"Closest number to {N} with all even digits: {closestNum}");
+
+            N = -33;
+            closestNum = finder.FindClosestEvenNumber(N);
+            Console.WriteLine($"Closest number to {N} with all even digits: {closestNum}");
+
+            Console.ReadLine();
+        }
+    }
+}
